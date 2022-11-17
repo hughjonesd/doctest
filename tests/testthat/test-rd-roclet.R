09135cd4c7a41d4cec1943865098b19317c9993b
David Hugh-Jones <davidhughjones@gmail.com>
2022-11-17 13:11:24 +0000

    testing package

--- tests/testthat/test-rd-roclet.R
+++ tests/testthat/test-rd-roclet.R
@@ -1,7 +1,15 @@
 
 
-test_that("parse_file", {
-  pkg <- testthat::testing_package()
-  result <- roxygen2::parse_file("safe-mean.Rmd")
+test_that("rd_roclet", {
+  r_file <- file.path("testPackage", "R", "safe-arithmetic.R")
+  r_file_text <- readLines(r_file)
 
+  roxygen2::load_options("testPackage")
+  expect_silent(
+      result <- roxygen2::parse_file(r_file)
+  )
+
+  expect_silent(
+    roxygen2::roc_proc_text(roxygen2::rd_roclet(), r_file_text)
+  )
 })
---
+++ tests/testthat/testPackage/R/safe-arithmetic.R
@@ -0,0 +1,44 @@
+#' Safe mean
+#'
+#' @param x Numeric vector
+#'
+#' @return Mean of `x`
+#'
+#' @export
+#'
+#' @examples
+#' x <- 1:3
+#' @expect equals(2)
+#' safe_mean(x)
+#'
+#' @test safe-mean-errors
+#' @expect error("not numeric")
+#' safe_mean("a")
+#'
+#' @expect warning("NA elements")
+#' safe_mean(c(1, NA))
+safe_mean <- function (x) {
+  if (length(x) == 0L) stop("No elements in x")
+  if (! is.numeric(x)) stop("x is not numeric")
+  if (any(is.na(x))) warning("x contains NA elements")
+  mean(x)
+}
+
+
+#' Safe variance
+#'
+#' @param x
+#'
+#' @return Variance of `x`
+#' @export
+#'
+#' @examples
+#' x <- 1:5
+#' @expect length(1)
+#' safe_var(x)
+safe_var <- function (x) {
+  if (length(x) == 0L) stop("No elements in x")
+  if (! is.numeric(x)) stop("x is not numeric")
+  if (any(is.na(x))) warning("x contains NA elements")
+  var(x)
+}
