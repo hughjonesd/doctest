cb19fafb8c4477e76882a1232fa9c01d33dca6c0
David Hugh-Jones <davidhughjones@gmail.com>
2022-11-21 12:33:22 +0000

    Use merge.rd_section to finalize examples (incomplete)

--- R/parse.R
+++ R/parse.R
@@ -10,7 +10,7 @@ roxy_tag_parse.roxy_tag_expect <- function (x) {
     roxygen2::warn_roxy_tag(x, "has no expectation defined")
   }
 
-  x <- tag_nonempty_examples(x)
+  # x <- tag_nonempty_examples(x)
 
   x
 }
@@ -33,7 +33,7 @@ roxy_tag_parse.roxy_tag_expectRaw <- function (x) {
 roxy_tag_parse.roxy_tag_doctest <- function (x) {
   x <- strip_first_line(x, first_line_name = "test_name")
 
-  x <- tag_nonempty_examples(x)
+  # x <- tag_nonempty_examples(x)
 
   x
 }
@@ -42,7 +42,7 @@ roxy_tag_parse.roxy_tag_doctest <- function (x) {
 #' @export
 roxy_tag_parse.roxy_tag_skipTest <- function (x) {
   x <- strip_first_line(x)
-  x <- tag_nonempty_examples(x)
+  # x <- tag_nonempty_examples(x)
 
   x
 }
@@ -51,7 +51,7 @@ roxy_tag_parse.roxy_tag_skipTest <- function (x) {
 #' @export
 roxy_tag_parse.roxy_tag_resumeTest <- function (x) {
   x <- strip_first_line(x)
-  x <- tag_nonempty_examples(x)
+  # x <- tag_nonempty_examples(x)
 
   x
 }
@@ -60,7 +60,7 @@ roxy_tag_parse.roxy_tag_resumeTest <- function (x) {
 #' @export
 roxy_tag_parse.roxy_tag_testComments <- function (x) {
   x <- strip_first_line(x)
-  x <- tag_nonempty_examples(x)
+  # x <- tag_nonempty_examples(x)
 
   x
 }
@@ -84,10 +84,11 @@ strip_first_line <- function (x, first_line_name = NULL) {
   x
 }
 
-tag_nonempty_examples <- function (x) {
-  if (length(x$raw) > 0 && stringr::str_trim(x$raw) != "") {
-    x <- roxygen2::tag_examples(x)
-  }
+
+#' @export
+roxy_tag_rd.roxy_tag_doctest <- function(x, base_path, env) {
+  x <- roxygen2::rd_section("doctest", x$raw)
+  x$is_doctest_tag <- TRUE
 
   x
 }
@@ -95,7 +96,10 @@ tag_nonempty_examples <- function (x) {
 
 #' @export
 roxy_tag_rd.roxy_tag_expect <- function(x, base_path, env) {
-  roxygen2::rd_section("examples", x$val)
+  x <- roxygen2::rd_section("doctest", x$raw)
+  x$is_doctest_tag <- FALSE
+
+  x
 }
 
 
@@ -104,16 +108,73 @@ roxy_tag_rd.roxy_tag_expectRaw <- roxy_tag_rd.roxy_tag_expect
 
 
 #' @export
-roxy_tag_rd.roxy_tag_doctest <- roxy_tag_rd.roxy_tag_expect
+roxy_tag_rd.roxy_tag_skipTest <- function(x, base_path, env) {
+  x <- roxygen2::rd_section("doctest", x$raw)
+  x$is_doctest_tag <- FALSE
 
+  x
+}
 
-#' @export
-roxy_tag_rd.roxy_tag_skipTest <- roxy_tag_rd.roxy_tag_expect
-
-
-#' @export
-roxy_tag_rd.roxy_tag_resumeTest <- roxy_tag_rd.roxy_tag_expect
 
 
 #' @export
-roxy_tag_rd.roxy_tag_testComments <- roxy_tag_rd.roxy_tag_expect
+roxy_tag_rd.roxy_tag_resumeTest <- function(x, base_path, env) {
+  x <- roxygen2::rd_section("doctest", x$raw)
+  x$is_doctest_tag <- FALSE
+
+  x
+}
+
+
+#' @export
+roxy_tag_rd.roxy_tag_testComments <- function(x, base_path, env) {
+  x <- roxygen2::rd_section("doctest", x$raw)
+  x$is_doctest_tag <- FALSE
+
+  x
+}
+
+
+#' @export
+merge.rd_section_doctest <- function (x, y, ...) {
+  x_is_doctest_tag <- x$is_doctest_tag
+  x <- NextMethod()
+  x$is_doctest_tag <- x_is_doctest_tag
+
+  if (! is.null(x$is_doctest_tag) && x$is_doctest_tag) {
+    # try to recreate ourselves, but with parsed Rd
+    raw <- paste(x$value, collapse = "\n")
+    if (raw != "") {
+      tmp_tag <- roxy_tag("roxy_tag_doctest", raw = raw)
+      tmp_tag <- roxygen2::tag_examples(tmp_tag)
+      if (! is.null(tmp_tag)) {
+        x$value <- tmp_tag$val
+      }
+    }
+  } else if (is.null(x$is_doctest_tag)) {
+    recover()
+  }
+
+  x
+}
+
+#' @export
+format.rd_section_doctest <- function (x, ...) {
+  value <- paste0(x$value, collapse = "\n")
+  roxygen2:::rd_macro("examples", value, space = TRUE)
+}
+
+# THE PROBLEM
+# ===========
+# - if roxy_tag_rd returns an rd_section("examples"),
+#   - then it gets merged nicely with the real @examples section...
+#   - BUT any tags that aren't at top level cause a "mismatched braces"
+#     error or similar, and we get no example section in the Rd
+# - if roxy_tag_rd returns its own kind of rd_section(),
+#   - then we can merge all the sections nicely with each other...
+#   - BUT they dont' get merged with the real examples section
+#
+# One radical solution is to *replace* the @examples tag with a doctest
+# tag. This then does all the work. It might seem a bit scary to people?
+# But it would allow for use of ordinary tags in the middle of the prose
+# and we'd cut down on the crap...!
