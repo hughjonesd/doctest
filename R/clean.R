9d4cf5b9ca48356394bf0cc5359719fbf340b066
David Hugh-Jones <davidhughjones@gmail.com>
2022-11-24 13:32:04 +0000

    Don't read non-testthat files in clean()

--- R/clean.R
+++ R/clean.R
@@ -2,15 +2,11 @@
 
 #' @export
 roclet_clean.roclet_doctest <- function (x, base_path) {
-  test_dir <- file.path(base_path, "tests", "testthat")
-  test_files <- list.files(test_dir, full.names = TRUE)
-
-  test_files <- test_files[! file.info(test_files)$isdir]
-
+  test_path <- file.path(base_path, "tests", "testthat")
+  test_files <- list.files(test_path, pattern = "^test-doctest-.*\\.[rR]$",
+                           full.names = TRUE)
   test_files <- purrr::keep(test_files, made_by_doctest)
-  test_files <- purrr::keep(test_files,
-                              \(f) grepl("^test-doctest-", basename(f))
-                            )
+
   unlink(test_files)
 }
 
